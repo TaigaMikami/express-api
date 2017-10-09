731ba4e2ad733a0cc987c527ed42754475f16020
TaigaMikami <mikamimikami60@gmail.com>
2017-10-09 14:47:59 +0000

    DOM操作

--- app/entry.js
+++ app/entry.js
@@ -1,7 +1,16 @@
 'use strict';
-const dc = require('damage-calc');
-document.writeln(
-  '<p>攻撃 100, 防御 50, 防御貫通 30 のダメージは、' +
-  dc.effectiveDamage(100, 50, 30) + '</p>');
-document.writeln(
-  '<p>' + require('crypto').randomBytes(8).toString('hex') + '</p>');
+
+const $ = require('jquery');
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingButton.click(() => {
+  block.animate({'marginLeft': '500px'}, 500);
+  block.animate({'marginLeft': '20px'}, 1000);
+});
